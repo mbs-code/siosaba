a37b88f4ee9a721987093de2be4da4c94e26dfec
mabasasi <aoe317293@yahoo.co.jp>
2020-02-05 18:12:59 +0900

    [fix] subscriber のログを出力しないように

--- database/subscriber/ChannelSubscriber.ts
+++ database/subscriber/ChannelSubscriber.ts
@@ -15,23 +15,23 @@ export class ChannelSubscriber implements EntitySubscriberInterface<Channel> {
   }
 
   async afterInsert (event: InsertEvent<Channel>) {
-    console.log('> channel subscriber - after insert')
+    // console.log('> channel subscriber - after insert')
 
     const channel = event.entity
 
     // meta の保存
     const meta = channel.createChannelMeta()
     await event.queryRunner.manager.save(meta)
-    console.log('>> save channel_meta')
+    // console.log('>> save channel_meta')
 
     // stat の保存
     const stat = channel.createChannelStat()
     await event.queryRunner.manager.save(stat)
-    console.log('>> save channel_stat')
+    // console.log('>> save channel_stat')
   }
 
   async afterUpdate (event: UpdateEvent<Channel>) {
-    console.log('> channel subscriber - after update')
+    // console.log('> channel subscriber - after update')
     const channel = event.entity
     const changeColumns = event.updatedColumns.map(e => e.propertyName)
 
@@ -39,12 +39,12 @@ export class ChannelSubscriber implements EntitySubscriberInterface<Channel> {
     if (ChannelMeta.isOwnColumn(...changeColumns)) {
       const meta = channel.createChannelMeta()
       await event.queryRunner.manager.save(meta)
-      console.log('>> save channel_meta')
+      // console.log('>> save channel_meta')
     }
 
     // stat は常時更新
     const stat = channel.createChannelStat()
     await event.queryRunner.manager.save(stat)
-    console.log('>> save channel_stat')
+    // console.log('>> save channel_stat')
   }
 }
