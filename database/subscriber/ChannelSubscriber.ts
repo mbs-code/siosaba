90e235eb96ce6e326b2534dd3810ae50643ede32
mabasasi <aoe317293@yahoo.co.jp>
2020-04-14 19:30:02 +0900

    [mod] subscriber の保存機構を record 形式に変更

--- database/subscriber/ChannelSubscriber.ts
+++ database/subscriber/ChannelSubscriber.ts
@@ -2,13 +2,15 @@ import {
   EventSubscriber,
   EntitySubscriberInterface, // eslint-disable-line no-unused-vars
   InsertEvent, // eslint-disable-line no-unused-vars
-  UpdateEvent // eslint-disable-line no-unused-vars
+  UpdateEvent, // eslint-disable-line no-unused-vars
+  QueryRunner // eslint-disable-line no-unused-vars
 } from 'typeorm'
 
 import { cli as Logger } from '../../src/lib/logger'
 
 import { Channel } from '../entity/Channel'
-import { ChannelMeta } from '../entity/ChannelMeta'
+import { ChannelRecord } from './../entity/ChannelRecord'
+import { ChannelStat } from './../entity/ChannelStat'
 
 @EventSubscriber()
 export class ChannelSubscriber implements EntitySubscriberInterface<Channel> {
@@ -17,34 +19,65 @@ export class ChannelSubscriber implements EntitySubscriberInterface<Channel> {
   }
 
   async afterInsert (event: InsertEvent<Channel>) {
+    const qr = event.queryRunner
     const channel = event.entity
 
-    // meta の保存
-    const meta = channel.createChannelMeta()
-    await event.queryRunner.manager.save(meta)
-    Logger.trace(`> - save channel_meta { id: ${meta.id} }`)
-
-    // stat の保存
-    const stat = channel.createChannelStat()
-    await event.queryRunner.manager.save(stat)
-    Logger.trace(`> - save channel_stat { id: ${stat.id} }`)
+    // ■ stat に新しい値を追加
+    await this.saveStat(qr, channel)
   }
 
   async afterUpdate (event: UpdateEvent<Channel>) {
+    const qr = event.queryRunner
     const channel = event.entity
-    const changeColumns = event.updatedColumns.map(e => e.propertyName)
+    const oldChannel = event.databaseEntity
+
+    // 変更カラム名の抽出 (id, createdAt, updatedAt は除外される)
+    const changeColumns = event.updatedColumns.map(e => e.propertyName) // id, createdAt, updatedAt は除外される
     Logger.trace(`> - change: ${JSON.stringify(changeColumns)}`)
 
-    // meta は更新要素があったときだけ
-    if (ChannelMeta.isOwnColumn(...changeColumns)) {
-      const meta = channel.createChannelMeta()
-      await event.queryRunner.manager.save(meta)
-      Logger.trace(`> - save channel_meta { id: ${meta.id} }`)
+    // ■ record に変更点を記録
+    await this.saveRecord(qr, channel, oldChannel, changeColumns)
+
+    // ■ stat に新しい値を追加
+    await this.saveStat(qr, channel)
+  }
+
+  /// ////////////////////////////////////////////////////////////////////////////////
+
+  private async saveRecord (qr: QueryRunner, channel: Channel, oldChannel: Channel, changeColumns: string[]) {
+    const recordColumns = Channel.filterOnlyRecordColumns(changeColumns)
+    Logger.trace(`> - record: ${JSON.stringify(recordColumns)}`)
+
+    if (recordColumns.length > 0) {
+      const record = new ChannelRecord()
+      record.channelId = channel.id
+      record.json = {}
+      for (const column of changeColumns) {
+        record.json[column] = [oldChannel[column], channel[column]]
+      }
+
+      await qr.manager.save(record)
+      Logger.trace(`> - save channel_record { id: ${record.id || -1} }`)
+      // console.log(record)
+      return record
     }
 
-    // stat は常時更新
-    const stat = channel.createChannelStat()
-    await event.queryRunner.manager.save(stat)
-    Logger.trace(`> - save channel_stat { id: ${stat.id} }`)
+    return null
+  }
+
+  private async saveStat (qr: QueryRunner, channel: Channel) {
+    const statColumns = ChannelStat.getColumnNames('id', 'channelId', 'createdAt')
+    Logger.trace(`> - stat: ${JSON.stringify(statColumns)}`)
+
+    const stat = new ChannelStat()
+    stat.channelId = channel.id
+    for (const column of statColumns) {
+      stat[column] = channel[column]
+    }
+
+    await qr.manager.save(stat)
+    Logger.trace(`> - save channel_stat { id: ${stat.id || -1} }`)
+    // console.log(stat)
+    return stat
   }
 }
--- database/subscriber/VideoSubscriber.ts
+++ database/subscriber/VideoSubscriber.ts
@@ -1,14 +1,17 @@
+
 import {
   EventSubscriber,
   EntitySubscriberInterface, // eslint-disable-line no-unused-vars
   InsertEvent, // eslint-disable-line no-unused-vars
-  UpdateEvent // eslint-disable-line no-unused-vars
+  UpdateEvent, // eslint-disable-line no-unused-vars
+  QueryRunner // eslint-disable-line no-unused-vars
 } from 'typeorm'
 
 import { cli as Logger } from '../../src/lib/logger'
 
 import { Video } from '../entity/Video'
-import { VideoMeta } from '../entity/VideoMeta'
+import { VideoRecord } from './../entity/VideoRecord'
+import { VideoStat } from './../entity/VideoStat'
 
 @EventSubscriber()
 export class VideoSubscriber implements EntitySubscriberInterface<Video> {
@@ -17,34 +20,65 @@ export class VideoSubscriber implements EntitySubscriberInterface<Video> {
   }
 
   async afterInsert (event: InsertEvent<Video>) {
+    const qr = event.queryRunner
     const video = event.entity
 
-    // meta の保存
-    const meta = video.createVideoMeta()
-    await event.queryRunner.manager.save(meta)
-    Logger.trace(`> - save video_meta { id: ${meta.id} }`)
-
-    // stat の保存
-    const stat = video.createVideoStat()
-    await event.queryRunner.manager.save(stat)
-    Logger.trace(`> - save video_stat { id: ${stat.id} }`)
+    // ■ stat に新しい値を追加
+    await this.saveStat(qr, video)
   }
 
   async afterUpdate (event: UpdateEvent<Video>) {
+    const qr = event.queryRunner
     const video = event.entity
+    const oldVideo = event.databaseEntity
+
+    // 変更カラム名の抽出 (id, createdAt, updatedAt は除外される)
     const changeColumns = event.updatedColumns.map(e => e.propertyName)
     Logger.trace(`> - change: ${JSON.stringify(changeColumns)}`)
 
-    // meta は更新要素があったときだけ
-    if (VideoMeta.isOwnColumn(...changeColumns)) {
-      const meta = video.createVideoMeta()
-      await event.queryRunner.manager.save(meta)
-      Logger.trace(`> - save video_meta { id: ${meta.id} }`)
+    // ■ record に変更点を記録
+    await this.saveRecord(qr, video, oldVideo, changeColumns)
+
+    // ■ stat に新しい値を追加
+    await this.saveStat(qr, video)
+  }
+
+  /// ////////////////////////////////////////////////////////////////////////////////
+
+  private async saveRecord (qr: QueryRunner, video: Video, oldVideo: Video, changeColumns: string[]) {
+    const recordColumns = Video.filterOnlyRecordColumns(changeColumns)
+    Logger.trace(`> - record: ${JSON.stringify(recordColumns)}`)
+
+    if (recordColumns.length > 0) {
+      const record = new VideoRecord()
+      record.videoId = video.id
+      record.json = {}
+      for (const column of changeColumns) {
+        record.json[column] = [oldVideo[column], video[column]]
+      }
+
+      await qr.manager.save(record)
+      Logger.trace(`> - save video_record { id: ${record.id || -1} }`)
+      // console.log(record)
+      return record
     }
 
-    // stat は常時更新
-    const stat = video.createVideoStat()
-    await event.queryRunner.manager.save(stat)
-    Logger.trace(`> - save video_stat { id: ${stat.id} }`)
+    return null
+  }
+
+  private async saveStat (qr: QueryRunner, video: Video) {
+    const statColumns = VideoStat.getColumnNames('id', 'videoId', 'createdAt')
+    Logger.trace(`> - stat: ${JSON.stringify(statColumns)}`)
+
+    const stat = new VideoStat()
+    stat.videoId = video.id
+    for (const column of statColumns) {
+      stat[column] = video[column]
+    }
+
+    await qr.manager.save(stat)
+    Logger.trace(`> - save video_stat { id: ${stat.id || -1} }`)
+    // console.log(stat)
+    return stat
   }
 }
