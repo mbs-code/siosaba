eb0b46b6a2faab981d9bd263c3c6c06a3c5c47d8
mabasasi <aoe317293@yahoo.co.jp>
2020-04-18 08:24:31 +0900

    [add] アプリの変数を保存する機構を追加

---
+++ database/entity/App.ts
@@ -0,0 +1,34 @@
+import {
+  Entity,
+  Column,
+  Index,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  UpdateDateColumn
+} from 'typeorm'
+
+import { ExtendEntity } from './ExtendEntity'
+import { AppVariableKey } from './type/AppVariableKey'
+
+@Entity('apps')
+export class App extends ExtendEntity {
+  @PrimaryGeneratedColumn()
+  id: number
+
+  ///
+
+  @Index({ unique: true })
+  @Column({ type: 'enum', enum: AppVariableKey })
+  key: AppVariableKey
+
+  @Column({ nullable: true })
+  value: string
+
+  ///
+
+  @CreateDateColumn({ type: 'datetime', precision: 3, default: () => 'CURRENT_TIMESTAMP(3)', name: 'created_at' })
+  createdAt: Date
+
+  @UpdateDateColumn({ type: 'datetime', precision: 3, default: () => 'CURRENT_TIMESTAMP(3)', name: 'updated_at' })
+  updatedAt: Date
+}
